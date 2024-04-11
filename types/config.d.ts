dad0704ba4dfcbd88c579ba4808bde49a0da3550
hiep.nguyen@icomm.vn <hiep.nguyen@icomm.vn>
2024-04-11 14:16:39 +0700

    remove redundant env key

--- types/config.d.ts
+++ types/config.d.ts
@@ -25,12 +25,8 @@ export interface GlobConfig {
   shortName: string;
 }
 export interface GlobEnvConfig {
-  // Site title
-  VITE_GLOB_APP_TITLE: string;
   // Service interface url
   VITE_GLOB_API_URL: string;
-  // Service interface url prefix
-  VITE_GLOB_API_URL_PREFIX?: string;
   // Project abbreviation
   VITE_GLOB_APP_SHORT_NAME: string;
   // Upload url
