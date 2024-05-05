e4b37f5cfbb74232e4ad4491bd6233e8eb070017
hiep.nguyen <hiep.nguyen@icomm.vn>
2024-05-06 02:45:18 +0700

    apply physic

--- src/views/Showroom/index.vue
+++ src/views/Showroom/index.vue
@@ -9,6 +9,8 @@
   import { OrbitControls } from "three/addons/controls/OrbitControls.js";
   // import { Icon } from "/@/uikits/Icon";
   import { onMounted, ref } from "vue";
+  import * as CANNON from "cannon-es";
+  import CannonDebugger from "cannon-es-debugger";
 
   const el = ref<any>(null);
   const scene = new THREE.Scene();
@@ -25,7 +27,7 @@
   scene.add(gridHelper, axesHelper);
 
   const groundGeo = new THREE.PlaneGeometry(1_000, 1_000, 32, 32);
-  const groundMat = new THREE.MeshStandardMaterial({
+  const groundMat = new THREE.MeshBasicMaterial({
     color: 0xd5d5d5,
     side: THREE.DoubleSide,
   });
@@ -33,25 +35,65 @@
   ground.rotateX(Math.PI / 2);
   scene.add(ground);
 
-  onMounted(() => {
+  const sphereMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 16, 16),
+    new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+    })
+  );
+  scene.add(sphereMesh);
+
+  // PHYSIC
+  const physicWorld = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.82, 0), // m/s²
+  });
+  const groundBody = new CANNON.Body({
+    type: CANNON.Body.STATIC, // can also be achieved by setting the mass to 0
+    shape: new CANNON.Plane(),
+  });
+  groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+  physicWorld.addBody(groundBody);
+  // BODY
+  const sphereBody = new CANNON.Body({
+    mass: 5,
+    shape: new CANNON.Sphere(1),
+  });
+  sphereBody.position.set(0, 10, 0);
+  const boxBody = new CANNON.Body({
+    mass: 7,
+    shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+  });
+  boxBody.position.set(1, 15, 1);
+  physicWorld.addBody(sphereBody);
+  physicWorld.addBody(boxBody);
+
+  // CANNON DEBUGGER
+  const cannonDebugger = new CannonDebugger(scene, physicWorld, {});
+
+  onMounted(async () => {
     scene.background = new THREE.Color("black");
+
     if (el.value) {
       const { width, height } = el.value.getBoundingClientRect();
       camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1_000_000);
-      camera.position.setZ(10);
+      camera.position.setZ(20);
+      camera.position.setY(20);
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(width, height);
       el.value.appendChild(renderer.domElement);
       orbitControls = new OrbitControls(camera, renderer.domElement);
       orbitControls.enableDamping = true;
     }
-
     animate();
   });
   function render(scene: any, camera: any) {
     renderer.render(scene, camera);
   }
   function animate() {
+    physicWorld.fixedStep();
+    cannonDebugger.update();
+    sphereMesh.position.copy(sphereBody.position);
+    sphereMesh.quaternion.copy(sphereBody.quaternion);
     requestAnimationFrame(animate);
     orbitControls.update();
     render(scene, camera);
