cb046b6f9d76b9b5c5ad3fde758503a76c95d017
hiep.nguyen <hiep.nguyen@icomm.vn>
2024-05-15 02:15:46 +0700

    Updates

--- assets/index.336a3e16.js
+++
@@ -1,5 +0,0 @@
-import{b as o,ai as r,r as i,j as l,g as u,e as c,aj as f,u as p,o as n}from"./vendor.289a3f5a.js";const _=o({__name:"index",setup(d){const s=r(),e=i(null);function t(a){e.value&&e.value.style&&(e.value.style.background=`radial-gradient(
-          1000px at ${a.clientX}px ${a.clientY}px,
-          rgba(29, 78, 216, 0.15),
-          transparent 80%
-        )`)}return(a,m)=>(n(),l("div",{class:"w-full h-full",onMousemove:t},[u("div",{ref_key:"mouseHighlight",ref:e,class:"fixed inset-0 z-30 transition duration-300 pointer-events-none"},null,512),(n(),c(f(p(s).params.key)))],32))}});export{_ as default};
